1007b2689bdd440fd3ead65d6c30d9dab9e64247
solaire <22472584+Solaire@users.noreply.github.com>
2026-01-04 23:06:32 +0000

    temporary

---
+++ perl/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Source - https://stackoverflow.com/questions/2980297/how-can-i-use-cpan-as-a-non-root-user
+# Posted by Chas. Owens, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-01-04, License - CC BY-SA 4.0
+
+wget -O- http://cpanmin.us | perl - -l ~/perl5 App::cpanminus local::lib
+eval `perl -I ~/perl5/lib/perl5 -Mlocal::lib`
+echo 'eval `perl -I ~/perl5/lib/perl5 -Mlocal::lib`' >> ~/.bashrc
+echo 'export MANPATH=$HOME/perl5/man:$MANPATH' >> ~/.bashrc
